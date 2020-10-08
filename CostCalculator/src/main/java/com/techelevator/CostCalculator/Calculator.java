85fdfc746999b6a1f873fb97a66e5cc2b8d30a3d
Student <student@Techs-MacBook-Air.local>
2020-10-07 22:52:46 -0400

    Created lists for schools, programs, and levels

---
+++ CostCalculator/src/main/java/com/techelevator/CostCalculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.techelevator.CostCalculator;
+
+public class Calculator {
+
+}
+
+
+// ask user for school, degree, and program
+
+System.out.println("Select school: ");
+for (int i = 0; i < quizQuestion.getAnswers().length; i++) {
+	System.out.println(quizQuestion.getAnswers()[i]);
+
+
+System.out.println("Select program of study: ");
+System.out.println("Select education level: ");
+
+
+
+// pull data from other classes
+
+// input scholarship/assistantship
+
+// display tuition, discount, room, board, health insurance, 
+// personal expenses, fee for new student, and additional fees
+
+// output estimated cost before scholarships, scholarship amount,
+// and total cost
